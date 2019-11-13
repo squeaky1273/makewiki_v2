c662b4ff78acc5f7dc70045d45dd2967f4a3fe05
squeaky1273 <pjriddell22@gmail.com>
2019-11-13 10:01:55 -0800

    added code

---
+++ wiki/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from wiki.models import Page
+
+
+class PageListView(ListView):
+    """ Renders a list of all Pages. """
+    model = Page
+
+    def get(self, request):
+        """ GET a list of Pages. """
+        pages = self.get_queryset().all()
+        return render(request, 'list.html', {
+          'pages': pages
+        })
+
+class PageDetailView(DetailView):
+    """ Renders a specific page based on it's slug."""
+    model = Page
+
+    def get(self, request, slug):
+        """ Returns a specific wiki page by slug. """
+        page = self.get_queryset().get(slug__iexact=slug)
+        return render(request, 'page.html', {
+          'page': page
+        })
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
